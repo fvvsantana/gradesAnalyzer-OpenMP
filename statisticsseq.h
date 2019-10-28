889cf714006cdc095f155b7ef8a9fa537e1359fe
Flavio Santana <fvvsantana@gmail.com>
2019-10-28 19:20:16 -0300

    Add double versions of the statistics functions

--- statisticsseq.h
+++ statisticsseq.h
@@ -7,4 +7,10 @@ double find_median(int* grades, int nStudents, int range);
 double calculate_average(int* grades, int nStudents);
 double calculate_stddev(int* grades, int nStudents);
 
+double find_min_double(double* grades, int nStudents);
+double find_max_double(double* grades, int nStudents);
+double find_median_double(double* grades, int nStudents, int range);
+double calculate_average_double(double* grades, int nStudents);
+double calculate_stddev_double(double* grades, int nStudents);
+
 #endif // STATISTICSSEQ_H
